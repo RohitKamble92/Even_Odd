6be8fa863f31dc887a2230741791d1b1796d56d8
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:40:57 +0530

    Welcome to even or odd numbers program

---
+++ src/com/bridgelabz/evenorodd/EvenorOdd.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.evenorodd;
+
+public class EvenorOdd {
+    public static void main(String[] args) {
+        System.out.println("Welcome to even and odd numbers program");
+    }
+}
